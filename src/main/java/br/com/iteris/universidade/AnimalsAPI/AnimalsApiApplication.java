2675b3eda784d6c5f344fc6a409cb32b2913af48
Bruna Campos Ferreira <bruna.ferreira@iteris.com.br>
2022-07-22 11:54:53 -0300

    initial config

---
+++ src/main/java/br/com/iteris/universidade/AnimalsAPI/AnimalsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.iteris.universidade.AnimalsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsApiApplication.class, args);
+	}
+
+}
